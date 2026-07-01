a81040c7f4305cfce923700d4066b4be4527fd81
Shubham Gupta <158546006+Shubham12gupta@users.noreply.github.com>
2026-07-02 01:16:38 +0530

    Update updatebackendnew.sh

--- Automations/updatebackendnew.sh
+++ Automations/updatebackendnew.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-# Set the Instance ID and path to the .env file
-INSTANCE_ID="i-030da7d31a1dbbffc"
+# DigitalOcean Worker Public IP
+PUBLIC_IP="134.209.146.20"
 
-# Retrieve the public IP address of the specified EC2 instance
-ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+# Path to the backend .env file
+FILE="../backend/.env.docker"
 
-# Path to the .env file
-file_to_find="../backend/.env.docker"
-
-# Check the current FRONTEND_URL in the .env file
-current_url=$(sed -n "4p" $file_to_find)
-
-# Update the .env file if the IP address has changed
-if [[ "$current_url" != "FRONTEND_URL=\"http://${ipv4_address}:5173\"" ]]; then
-    if [ -f $file_to_find ]; then
-        sed -i -e "s|FRONTEND_URL.*|FRONTEND_URL=\"http://${ipv4_address}:5173\"|g" $file_to_find
-    else
-        echo "ERROR: File not found."
-    fi
+# Update FRONTEND_URL
+if [ -f "$FILE" ]; then
+    sed -i "s|^FRONTEND_URL=.*|FRONTEND_URL=\"http://${PUBLIC_IP}:5173\"|g" "$FILE"
+    echo "FRONTEND_URL updated to http://${PUBLIC_IP}:5173"
+else
+    echo "ERROR: $FILE not found."
 fi
